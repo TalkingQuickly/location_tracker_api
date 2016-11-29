c73444b3251c9f225a3bce5c9fa1597af3555298
Ben Dixon <ben@hillsbede.co.uk>
2016-11-29 14:38:37 +0000

    adds: api route for getting recent locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,4 +9,20 @@ class LocationsController < ApplicationController
       render json: {}, status: 401
     end
   end
+
+  def index
+    render json: {
+      data: current_user
+        .locations
+        .order(created_at: 'desc')
+        .limit(100).collect do |location|
+        {
+          title: location.id,
+          timestamp: location.timestamp,
+          latitude: location.lat,
+          longitude: location.lng
+        }
+      end
+    }
+  end
 end
